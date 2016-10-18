46e4454324749bf5e97f73027cd29814f7d983f0
Johannes J. Schmidt <jo@die-tf.de>
2016-10-18 17:19:10 +0200

    test: $or queries with $eq shortcut
    Add a test for $or queries which use $eq shortcut:
    ```
    $or: [
    	{"name.first": "Nancy"},
    	{"name.first": "Mick"}
    ]
    ```
    
    This fails on the plugin, while it works on CouchDB 2.0.

--- test/test-suite-1/test.combinational.js
+++ test/test-suite-1/test.combinational.js
@@ -94,6 +94,45 @@ module.exports = function (dbType, context) {
         });
       });
 
+      it('does $or queries with eq shortcut', function () {
+        var db = context.db;
+        var index = {
+          "index": {
+            "fields": ["age"]
+          },
+          "name": "age-index",
+        "type": "json"
+        };
+
+        return db.createIndex(index).then(function () {
+          return db.bulkDocs([
+            { _id: '1', age: 75, name: {first: 'Nancy', surname: 'Sinatra'}},
+            { _id: '2', age: 40, name: {first: 'Eddie', surname: 'Vedder'}},
+            { _id: '3', age: 80, name: {first: 'John', surname: 'Fogerty'}},
+            { _id: '4', age: 76, name: {first: 'Mick', surname: 'Jagger'}},
+          ]);
+        }).then(function () {
+          return db.find({
+            selector: {
+              $or: [
+                {"name.first": "Nancy"},
+                {"name.first": "Mick"}
+              ]
+            }
+          });
+        }).then(function (resp) {
+          var docs = resp.docs.map(function (doc) {
+            delete doc._rev;
+            return doc;
+          });
+
+          docs.should.deep.equal([
+              { _id: '1', age: 75, name: {first: 'Nancy', surname: 'Sinatra'}},
+              { _id: '4', age: 76, name: {first: 'Mick', surname: 'Jagger'}}
+            ]);
+        });
+      });
+
     });
 
     describe('$nor', function () {
